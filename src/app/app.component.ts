3814e98aac55a5103d35f4b023923b72c530ddc8
Kim Younghwan <tomato.202404.kimyoungh@gmail.com>
2024-08-27 16:41:20 +0900

    docs: Update understanding of AppModule and component declarations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ServerComponent } from './server/server.component';  // 변환된 standalone 컴포넌트
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ 
-    RouterOutlet
-  ],
+  imports: [RouterOutlet, ServerComponent],  // standalone 컴포넌트를 직접 임포트
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] 
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   name = 'kim';
