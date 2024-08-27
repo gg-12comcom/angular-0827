beb956a5dc745bab3f97e4bc1a856c94c713d09c
Kim Younghwan <tomato.202404.kimyoungh@gmail.com>
2024-08-27 14:43:46 +0900

    npm install --save bootstrap@3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, FormsModule ],
+  imports: [ 
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] 
 })
