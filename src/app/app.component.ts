58a6b2a293876b33f15f0eb2304a7756760b728b
Kim Younghwan <tomato.202404.kimyoungh@gmail.com>
2024-08-27 17:29:49 +0900

    Component Nesting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ServerComponent } from './server/server.component';  // 변환된 standalone 컴포넌트
+import { ServersComponent } from './servers/servers.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ServerComponent],  // standalone 컴포넌트를 직접 임포트
+  imports: [
+    RouterOutlet,
+    ServerComponent,
+    ServersComponent
+],  // standalone 컴포넌트를 직접 임포트
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ServerComponent } from "../server/server.component";
+
+@Component({
+  selector: 'app-servers',
+  standalone: true,
+  imports: [ServerComponent], 
+  // imports: [ServerComponent], <app-server> 태그를 사용할 수 있게 함
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+
+}
