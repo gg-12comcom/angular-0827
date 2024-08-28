b9cc5dcd11434713505c6988980f004851e9e5d1
Kim Younghwan <tomato.202404.kimyoungh@gmail.com>
2024-08-28 15:41:27 +0900

    serverCreated

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,30 @@
 import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: 'app-server',
     standalone: true,
-    templateUrl: './server.component.html'
+    imports: [CommonModule], // 여기에도 CommonModule을 추가
+    templateUrl: './server.component.html',
+    styles: [`
+        .online{
+            color: white;
+        }
+    `]
 })
 export class ServerComponent {
+    serverId: number = 10;
+    serverStatus: string = 'offline';
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+// getColor는 CommonModule을 추가해야 import 해야만 한다
+    getColor(){
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // ngModel를 사용하기 위해 추가 강의에는 없음
 import { ServerComponent } from "../server/server.component";
+import { CommonModule } from '@angular/common'; // ngIf를 사용하기 위해 CommonModule를 추가 강의에는 없음
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]',
+  // selector: '.app-servers',
   standalone: true,
-  imports: [ServerComponent], 
-  // imports: [ServerComponent], <app-server> 태그를 사용할 수 있게 함
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
-  styleUrl: './servers.component.css'
-})
-export class ServersComponent {
+  imports: [ServerComponent, FormsModule, CommonModule], 
 
+  // imports: [ServerComponent], <app-server> 태그를 사용할 수 있게 함
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']  // styleUrl -> styleUrls로 수정
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created! Name is';
+  serverName = 'Tesserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver2']
+
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {} // void {}를 넣어야 작동함. 강의와는 다른 부분.
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
